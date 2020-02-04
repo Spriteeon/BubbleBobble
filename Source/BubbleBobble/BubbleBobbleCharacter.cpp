5ee79733b816a14129b2dd3e1969bdbd2f1d1b72
Rodrigo Roldan Romero <luisrodrigoroldan@hotmail.com>
2020-02-04 12:01:58 +0000

    Disabled the player camera
    The normal fixed camera still works

--- Source/BubbleBobble/BubbleBobbleCharacter.cpp
+++ Source/BubbleBobble/BubbleBobbleCharacter.cpp
@@ -29,25 +29,25 @@ ABubbleBobbleCharacter::ABubbleBobbleCharacter()
 	GetCapsuleComponent()->SetCapsuleRadius(40.0f);
 
 	// Create a camera boom attached to the root (capsule)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	/*CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 500.0f;
 	CameraBoom->SocketOffset = FVector(0.0f, 0.0f, 75.0f);
 	CameraBoom->bAbsoluteRotation = true;
 	CameraBoom->bDoCollisionTest = false;
-	CameraBoom->RelativeRotation = FRotator(0.0f, -90.0f, 0.0f);
+	CameraBoom->RelativeRotation = FRotator(0.0f, -90.0f, 0.0f);*/
 	
 
 	// Create an orthographic camera (no perspective) and attach it to the boom
-	SideViewCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("SideViewCamera"));
+	/*SideViewCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("SideViewCamera"));
 	SideViewCameraComponent->ProjectionMode = ECameraProjectionMode::Orthographic;
 	SideViewCameraComponent->OrthoWidth = 2048.0f;
-	SideViewCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	SideViewCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);*/
 
 	// Prevent all automatic rotation behavior on the camera, character, and camera component
-	CameraBoom->bAbsoluteRotation = true;
+	/*CameraBoom->bAbsoluteRotation = true;
 	SideViewCameraComponent->bUsePawnControlRotation = false;
-	SideViewCameraComponent->bAutoActivate = true;
+	SideViewCameraComponent->bAutoActivate = true;*/
 	GetCharacterMovement()->bOrientRotationToMovement = false;
 
 	// Configure character movement
