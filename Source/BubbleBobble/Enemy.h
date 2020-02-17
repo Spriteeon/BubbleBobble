06da3d235239f1327d0493ee0bbe2106f90f5fcb
FranPorritt <francescaporritt1@gmail.com>
2020-02-17 12:58:54 +0000

    Enemy AI Behaviour Tree

--- Source/BubbleBobble/Enemy.h
+++ Source/BubbleBobble/Enemy.h
@@ -1,33 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "GameFramework/Character.h"
-#include "Enemy.generated.h"
-
-UCLASS()
-class BUBBLEBOBBLE_API AEnemy : public ACharacter
-{
-	GENERATED_BODY()
-
-private:
-	float movement_delay;
-	bool move_dir;
-
-public:
-	// Sets default values for this character's properties
-	AEnemy();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-	void Movement();
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-
-
-};
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "GameFramework/Character.h"
+//#include "Enemy.generated.h"
+//
+//UCLASS()
+//class BUBBLEBOBBLE_API AEnemy : public ACharacter
+//{
+//	GENERATED_BODY()
+//
+//private:
+//	float movement_delay;
+//	bool move_dir;
+//
+//public:
+//	// Sets default values for this character's properties
+//	AEnemy();
+//
+//protected:
+//	// Called when the game starts or when spawned
+//	virtual void BeginPlay() override;
+//	void Movement();
+//
+//public:
+//	// Called every frame
+//	virtual void Tick(float DeltaTime) override;
+//
+//
+//
+//};
