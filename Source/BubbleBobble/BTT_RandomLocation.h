7a05ce097f9474bdcc2151922391ef39302667f1
FranPorritt <francescaporritt1@gmail.com>
2020-03-02 13:02:05 +0000

    Created behaviour for patrol and chase

---
+++ Source/BubbleBobble/BTT_RandomLocation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTT_RandomLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUBBLEBOBBLE_API UBTT_RandomLocation : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
