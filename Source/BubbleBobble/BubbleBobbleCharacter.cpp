f80e63de6997712411e6d63138cd62b1299b0267
Rodrigo Roldan Romero <luisrodrigoroldan@hotmail.com>
2020-04-18 13:17:14 +0100

    Added the lives back as a public UPROPERTY

--- Source/BubbleBobble/BubbleBobbleCharacter.cpp
+++ Source/BubbleBobble/BubbleBobbleCharacter.cpp
@@ -32,26 +32,6 @@ ABubbleBobbleCharacter::ABubbleBobbleCharacter()
 	GetCapsuleComponent()->SetCapsuleHalfHeight(96.0f);
 	GetCapsuleComponent()->SetCapsuleRadius(40.0f);
 
-	// Create a camera boom attached to the root (capsule)
-	/*CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 500.0f;
-	CameraBoom->SocketOffset = FVector(0.0f, 0.0f, 75.0f);
-	CameraBoom->bAbsoluteRotation = true;
-	CameraBoom->bDoCollisionTest = false;
-	CameraBoom->RelativeRotation = FRotator(0.0f, -90.0f, 0.0f);*/
-	
-
-	// Create an orthographic camera (no perspective) and attach it to the boom
-	/*SideViewCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("SideViewCamera"));
-	SideViewCameraComponent->ProjectionMode = ECameraProjectionMode::Orthographic;
-	SideViewCameraComponent->OrthoWidth = 2048.0f;
-	SideViewCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);*/
-
-	// Prevent all automatic rotation behavior on the camera, character, and camera component
-	/*CameraBoom->bAbsoluteRotation = true;
-	SideViewCameraComponent->bUsePawnControlRotation = false;
-	SideViewCameraComponent->bAutoActivate = true;*/
 	GetCharacterMovement()->bOrientRotationToMovement = false;
 
 	// Configure character movement
@@ -70,12 +50,6 @@ ABubbleBobbleCharacter::ABubbleBobbleCharacter()
 	// Note: This can cause a little floating when going up inclines; you can choose the tradeoff between better
 	// behavior on the edge of a ledge versus inclines by setting this to true or false
 	GetCharacterMovement()->bUseFlatBaseForFloorChecks = true;
-
-    // 	TextComponent = CreateDefaultSubobject<UTextRenderComponent>(TEXT("IncarGear"));
-    // 	TextComponent->SetRelativeScale3D(FVector(3.0f, 3.0f, 3.0f));
-    // 	TextComponent->SetRelativeLocation(FVector(35.0f, 5.0f, 20.0f));
-    // 	TextComponent->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f));
-    // 	TextComponent->SetupAttachment(RootComponent);
 
 	// Enable replication on the Sprite component so animations show up when networked
 	GetSprite()->SetIsReplicated(true);
